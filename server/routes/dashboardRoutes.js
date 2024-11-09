575e2755840cb955e1a28b889e679b800f03c5bd
chupichapa <pasionharold01@gmail.com>
2024-11-09 10:24:05 +0800

    dasd
    dasd

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -6,47 +6,49 @@ const auth = require('../middleware/auth');
 router.get('/dashboard-summary', auth, async (req, res) => {
     try {
 
+      const userId = req.user.id;
+
         const [totalExpenses] = await db.query(`
             SELECT COUNT(*) AS totalNumberOfExpenses 
-            FROM expenses`
-          );
+            FROM expenses 
+            WHERE user_id = ?`, [userId]);
           
-          const [totalLendings] = await db.query(`
-            SELECT COUNT(*) AS totalNumberOfLendings 
-            FROM lendings`
-          );
+            const [totalLendings] = await db.query(`
+              SELECT COUNT(*) AS totalNumberOfLendings 
+              FROM lendings 
+              WHERE user_id = ?`, [userId]);
 
         // Query for "This Month" expenses and lendings summaries
         const [thisMonthExpenses] = await db.query(`
-            SELECT SUM(expense_cost) AS totalExpenses, 
-                   COUNT(*) AS totalNumberOfExpenses 
-            FROM expenses 
-            WHERE MONTH(expense_date) = MONTH(CURDATE()) 
-            AND YEAR(expense_date) = YEAR(CURDATE())`
-        );
+          SELECT SUM(expense_cost) AS totalExpenses, 
+                 COUNT(*) AS totalNumberOfExpenses 
+          FROM expenses 
+          WHERE user_id = ? 
+          AND MONTH(expense_date) = MONTH(CURDATE()) 
+          AND YEAR(expense_date) = YEAR(CURDATE())`, [userId]);
 
-        const [thisMonthLendings] = await db.query(`
+          const [thisMonthLendings] = await db.query(`
             SELECT SUM(lending_amount) AS totalLendings, 
                    COUNT(*) AS totalNumberOfLendings 
             FROM lendings 
-            WHERE MONTH(lending_date) = MONTH(CURDATE()) 
-            AND YEAR(lending_date) = YEAR(CURDATE())`
-        );
+            WHERE user_id = ? 
+            AND MONTH(lending_date) = MONTH(CURDATE()) 
+            AND YEAR(lending_date) = YEAR(CURDATE())`, [userId]);
 
         // Query for "This Year" expenses and lendings summaries
         const [thisYearExpenses] = await db.query(`
-            SELECT SUM(expense_cost) AS thisYearExpenses, 
-                   COUNT(*) AS totalNumberOfExpenses 
-            FROM expenses 
-            WHERE YEAR(expense_date) = YEAR(CURDATE())`
-        );
+          SELECT SUM(expense_cost) AS thisYearExpenses, 
+                 COUNT(*) AS totalNumberOfExpenses 
+          FROM expenses 
+          WHERE user_id = ? 
+          AND YEAR(expense_date) = YEAR(CURDATE())`, [userId]);
 
-        const [thisYearLendings] = await db.query(`
+       const [thisYearLendings] = await db.query(`
             SELECT SUM(lending_amount) AS thisYearLendings, 
                    COUNT(*) AS totalNumberOfLendings 
             FROM lendings 
-            WHERE YEAR(lending_date) = YEAR(CURDATE())`
-        );
+            WHERE user_id = ? 
+            AND YEAR(lending_date) = YEAR(CURDATE())`, [userId]);
 
         // Combine monthly data for expenses and lendings
         const monthlyTransactionsData = Array(12).fill(0); // Initialize an array for 12 months
@@ -56,86 +58,82 @@ router.get('/dashboard-summary', auth, async (req, res) => {
             SELECT MONTH(expense_date) AS month, 
                    SUM(expense_cost) AS totalExpenseCost 
             FROM expenses 
-            WHERE YEAR(expense_date) = YEAR(CURDATE()) 
+            WHERE user_id = ? 
+            AND YEAR(expense_date) = YEAR(CURDATE()) 
             GROUP BY MONTH(expense_date)
-            ORDER BY month`
-        );
+            ORDER BY month`, [userId]);
 
         // Query for monthly lendings per month in the current year
         const [monthlyLendings] = await db.query(`
-            SELECT MONTH(lending_date) AS month, 
-                   SUM(lending_amount) AS totalLendingAmount 
-            FROM lendings 
-            WHERE YEAR(lending_date) = YEAR(CURDATE()) 
-            GROUP BY MONTH(lending_date)
-            ORDER BY month`
-        );
+          SELECT MONTH(lending_date) AS month, 
+                 SUM(lending_amount) AS totalLendingAmount 
+          FROM lendings 
+          WHERE user_id = ? 
+          AND YEAR(lending_date) = YEAR(CURDATE()) 
+          GROUP BY MONTH(lending_date)
+          ORDER BY month`, [userId]);
 
         const [categories] = await db.query(`
             SELECT e.category_id, c.category_name 
             FROM expenses e
             LEFT JOIN categories c ON e.category_id = c.id
-            GROUP BY e.category_id, c.category_name
-          `);
+            WHERE e.user_id = ?
+            GROUP BY e.category_id, c.category_name`, [userId]);
           
-          const categoryExpenses = await Promise.all(categories.map(async (category) => {
+        const categoryExpenses = await Promise.all(categories.map(async (category) => {
             const [expenses] = await db.query(`
-              SELECT SUM(expense_cost) AS totalExpenses 
-              FROM expenses 
-              WHERE category_id = ?
-            `, [category.category_id]);
+                SELECT SUM(expense_cost) AS totalExpenses 
+                FROM expenses 
+                WHERE category_id = ? 
+                AND user_id = ?`, [category.category_id, userId]);
             return {
-              label: category.category_name,
-              data: expenses[0].totalExpenses || 0,
+                label: category.category_name,
+                data: expenses[0].totalExpenses || 0,
             };
-          }));
+        }));
 
-        // Populate monthly data array
         monthlyExpenses.forEach(expense => {
-            const monthIndex = expense.month - 1; // Zero-based index
-            monthlyTransactionsData[monthIndex] += Number(expense.totalExpenseCost) || 0;
-        });
+          const monthIndex = expense.month - 1;
+          monthlyTransactionsData[monthIndex] += Number(expense.totalExpenseCost) || 0;
+      });
 
-        monthlyLendings.forEach(lending => {
-            const monthIndex = lending.month - 1; // Zero-based index
-            monthlyTransactionsData[monthIndex] += Number(lending.totalLendingAmount) || 0;
-        });
+      monthlyLendings.forEach(lending => {
+          const monthIndex = lending.month - 1;
+          monthlyTransactionsData[monthIndex] += Number(lending.totalLendingAmount) || 0;
+      });
 
-        // Calculate the total for the entire year by adding yearly expenses and lendings
-        const totalYearTransactions =
-            (Number(thisYearExpenses[0]?.thisYearExpenses) || 0) +
-            (Number(thisYearLendings[0]?.thisYearLendings) || 0);
+      const totalYearTransactions =
+          (Number(thisYearExpenses[0]?.thisYearExpenses) || 0) +
+          (Number(thisYearLendings[0]?.thisYearLendings) || 0);
 
-        // Calculate the total for this month by adding monthly expenses and lendings
-        const monthlyTransactions =
-            (Number(thisMonthExpenses[0]?.totalExpenses) || 0) +
-            (Number(thisMonthLendings[0]?.totalLendings) || 0);
+      const monthlyTransactions =
+          (Number(thisMonthExpenses[0]?.totalExpenses) || 0) +
+          (Number(thisMonthLendings[0]?.totalLendings) || 0);
 
-        // Response object
-        res.json({
-            thisMonth: {
-              totalExpenses: Number(thisMonthExpenses[0]?.totalExpenses) || 0,
-              totalNumberOfExpenses: Number(thisMonthExpenses[0]?.totalNumberOfExpenses) || 0,
-              totalLendings: Number(thisMonthLendings[0]?.totalLendings) || 0,
-              totalNumberOfLendings: Number(thisMonthLendings[0]?.totalNumberOfLendings) || 0,
-            },
-            thisYear: {
-              totalExpenses: Number(thisYearExpenses[0]?.thisYearExpenses) || 0,
-              totalNumberOfExpenses: Number(thisYearExpenses[0]?.totalNumberOfExpenses) || 0,
-              totalLendings: Number(thisYearLendings[0]?.thisYearLendings) || 0,
-              totalNumberOfLendings: Number(thisYearLendings[0]?.totalNumberOfLendings) || 0,
-            },
-            totalNumberOfExpenses: Number(totalExpenses[0]?.totalNumberOfExpenses) || 0,
-            totalNumberOfLendings: Number(totalLendings[0]?.totalNumberOfLendings) || 0,
-            totalYearTransactions,
-            monthlyTransactions,
-            monthlyTransactionsData,
-            categoryExpenses
-          });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-        console.log(error);
-    }
+      res.json({
+          thisMonth: {
+            totalExpenses: Number(thisMonthExpenses[0]?.totalExpenses) || 0,
+            totalNumberOfExpenses: Number(thisMonthExpenses[0]?.totalNumberOfExpenses) || 0,
+            totalLendings: Number(thisMonthLendings[0]?.totalLendings) || 0,
+            totalNumberOfLendings: Number(thisMonthLendings[0]?.totalNumberOfLendings) || 0,
+          },
+          thisYear: {
+            totalExpenses: Number(thisYearExpenses[0]?.thisYearExpenses) || 0,
+            totalNumberOfExpenses: Number(thisYearExpenses[0]?.totalNumberOfExpenses) || 0,
+            totalLendings: Number(thisYearLendings[0]?.thisYearLendings) || 0,
+            totalNumberOfLendings: Number(thisYearLendings[0]?.totalNumberOfLendings) || 0,
+          },
+          totalNumberOfExpenses: Number(totalExpenses[0]?.totalNumberOfExpenses) || 0,
+          totalNumberOfLendings: Number(totalLendings[0]?.totalNumberOfLendings) || 0,
+          totalYearTransactions,
+          monthlyTransactions,
+          monthlyTransactionsData,
+          categoryExpenses
+      });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+      console.log(error);
+  }
 });
 
 module.exports = router;
